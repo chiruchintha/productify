72418aecd5199f628178f46c954894d0fba6e92b
chiru417 <chiruchintha9@gmail.com>
2026-09-19 20:39:30 +0530

    add schema to the project

---
+++ src/db/schema.ts
@@ -0,0 +1,53 @@
+import { pgTable, PgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { Many, relations } from "drizzle-orm";
+
+export const users = pgTable("users", {
+    id: text("id").primaryKey(),
+    email: text("email").notNull().unique(),
+    name: text("name"),
+    imageUrl: text("image_url"),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt:timestamp("updated_at",{mode:"date"}).notNull().defaultNow(),
+})
+
+export const products = pgTable("products", {
+    id: uuid("id").defaultRandom().primaryKey(),
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    imageUrl: text("image_url").notNull(),
+    userId: text("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt:timestamp("updated_at",{mode:"date"}).notNull().defaultNow(),
+})
+
+export const comments = pgTable("comments",{
+    id: uuid("id").defaultRandom().primaryKey(),
+    content: text("content").notNull(),
+    userId: text("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+    productId: uuid("product_id").notNull().references(() => products.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
+})
+
+export const userRelations = relations(users, ({ many }) => ({
+    products: many(products),
+    comments:many(comments)
+}))
+
+export const productRelations = relations(products, ({ one, many }) => ({
+    users: one(users,{fields:[products.userId],references:[users.id]}),
+    comments:many(comments)
+}))
+
+export const commentsRelations = relations(comments, ({ many }) => ({
+    users: many(users),
+    products:many(products)
+}))
+
+export type User = typeof users.$inferSelect;
+export type Product = typeof products.$inferSelect;
+export type Comment = typeof comments.$inferSelect;
+
+export type NewUser = typeof users.$inferSelect;
+export type NewProduct = typeof products.$inferSelect;
+export type NewComment = typeof comments.$inferSelect;
